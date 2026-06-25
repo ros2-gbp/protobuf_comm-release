31a3172f4eff30f564a82fb28caff736bfdda63f
Tarik Viehmann <viehmann@kbsg.rwth-aachen.de>
2026-06-25 16:40:19 +0200

    Rebase from 'debian/lyrical/protobuf_comm'

--- qa/qa_server.cpp
+++ qa/qa_server.cpp
@@ -40,82 +40,63 @@
 #include <boost/asio.hpp>
 
 using namespace protobuf_comm;
-//using namespace llsf_msgs;
+// using namespace llsf_msgs;
 
 /// @cond QA
 
-static bool          quit = false;
+static bool quit = false;
 ProtobufStreamServer server(1234);
 
-void
-signal_handler(const boost::system::error_code &error, int signum)
-{
-	if (!error) {
-		quit = true;
-	}
-}
-
-void
-client_connected(ProtobufStreamServer::ClientID client, boost::asio::ip::tcp::endpoint &endpoint)
-{
-	printf("Client %u (%s:%u) connected\n",
-	       client,
-	       endpoint.address().to_string().c_str(),
-	       endpoint.port());
-}
-
-void
-client_disconnected(ProtobufStreamServer::ClientID client, const boost::system::error_code &error)
-{
-	if (!error) {
-		printf("Client %u disconnected\n", client);
-	} else {
-		printf("Client %u disconnected, error was: %s\n", client, error.message().c_str());
-	}
-}
-
-void
-handle_message(ProtobufStreamServer::ClientID             client,
-               uint16_t                                   component_id,
-               uint16_t                                   msg_type,
-               std::shared_ptr<google::protobuf::Message> msg)
-{
-	printf("Received message of type %u from %u\n", msg_type, client);
-	/*
-  std::shared_ptr<Person> p;
-  if ((p = std::dynamic_pointer_cast<Person>(msg))) {
-    printf("Person %i: %s <%s>\n", p->id(), p->name().c_str(), p->email().c_str());
+void signal_handler(const boost::system::error_code &error, int signum) {
+  if (!error) {
+    quit = true;
   }
-
-  server.send(client, component_id, msg_type, *p);
-  */
 }
 
-int
-main(int argc, char **argv)
-{
-	boost::asio::io_service io_service;
-
-	//MessageRegister & message_register = server.message_register();
-	//message_register.add_message_type<Person>(1, 2);
-
-	server.signal_connected().connect(client_connected);
-	server.signal_disconnected().connect(client_disconnected);
-	server.signal_received().connect(handle_message);
-
-	// Construct a signal set registered for process termination.
-	boost::asio::signal_set signals(io_service, SIGINT, SIGTERM);
-
-	// Start an asynchronous wait for one of the signals to occur.
-	signals.async_wait(signal_handler);
-
-	do {
-		io_service.run();
-		io_service.reset();
-	} while (!quit);
-
-	// Delete all global objects allocated by libprotobuf
-	google::protobuf::ShutdownProtobufLibrary();
+void client_connected(ProtobufStreamServer::ClientID client,
+                      boost::asio::ip::tcp::endpoint &endpoint) {
+  printf("Client %u (%s:%u) connected\n", client,
+         endpoint.address().to_string().c_str(), endpoint.port());
 }
 
+void client_disconnected(ProtobufStreamServer::ClientID client,
+                         const boost::system::error_code &error) {
+  if (!error) {
+    printf("Client %u disconnected\n", client);
+  } else {
+    printf("Client %u disconnected, error was: %s\n", client,
+           error.message().c_str());
+  }
+}
+
+void handle_message(ProtobufStreamServer::ClientID client,
+                    uint16_t component_id, uint16_t msg_type,
+                    std::shared_ptr<google::protobuf::Message> msg) {
+  printf("Received message of type %u from %u\n", msg_type, client);
+  /*
+std::shared_ptr<Person> p;
+if ((p = std::dynamic_pointer_cast<Person>(msg))) {
+printf("Person %i: %s <%s>\n", p->id(), p->name().c_str(), p->email().c_str());
+}
+server.send(client, component_id, msg_type, *p);
+*/
+}
+int main(int argc, char **argv) {
+  boost::asio::io_context io_context;
+  // MessageRegister & message_register = server.message_register();
+  // message_register.add_message_type<Person>(1, 2);
+  server.signal_connected().connect(client_connected);
+  server.signal_disconnected().connect(client_disconnected);
+  server.signal_received().connect(handle_message);
+  // Construct a signal set registered for process termination.
+  boost::asio::signal_set signals(io_context, SIGINT, SIGTERM);
+  // Start an asynchronous wait for one of the signals to occur.
+  signals.async_wait(signal_handler);
+  do {
+    io_context.run();
+    io_context.restart();
+  } while (!quit);
+  // Delete all global objects allocated by libprotobuf
+  google::protobuf::ShutdownProtobufLibrary();
+}
 /// @endcond
